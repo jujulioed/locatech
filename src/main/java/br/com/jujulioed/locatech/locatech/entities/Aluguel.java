26b706edca270e00a1adb883e85b1a713059182e
jujulioed <julioeduardon9@hotmail.com>
2025-05-07 19:24:44 -0300

    Create the entities
     - Created the vehicle entity;
     - Created the person entity;
     - Created the rent entity;
     - Used the lombok lib to facilitate the creation of Getters, Setters
       and Contructors.
    
    Signed-off-by: jujulioed <julioeduardon9@hotmail.com>

---
+++ src/main/java/br/com/jujulioed/locatech/locatech/entities/Aluguel.java
@@ -0,0 +1,23 @@
+package br.com.jujulioed.locatech.locatech.entities;
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Aluguel {
+    private Long id;
+    private Long pessoaId;
+    private Long veiculoId;
+    private String veiculoModelo;
+    private String pessoaCpf;
+    private String pessoaNome;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private BigDecimal valorTotal;
+
+}
