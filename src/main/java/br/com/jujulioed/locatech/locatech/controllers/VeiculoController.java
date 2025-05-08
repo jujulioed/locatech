185784e0689cc9b9b9b929a6f1873fbd2accf0bd
jujulioed <julioeduardon9@hotmail.com>
2025-05-07 21:56:33 -0300

    Create Veiculo Controller p1
     - Created the firsts parts of Veiculo Controller layer
    
    Signed-off-by: jujulioed <julioeduardon9@hotmail.com>

--- src/main/java/br/com/jujulioed/locatech/locatech/controllers/VeiculoController.java
+++ src/main/java/br/com/jujulioed/locatech/locatech/controllers/VeiculoController.java
@@ -1,15 +1,48 @@
 package br.com.jujulioed.locatech.locatech.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.jujulioed.locatech.locatech.entities.Veiculo;
+import br.com.jujulioed.locatech.locatech.services.VeiculoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/veiculos")
 public class VeiculoController {
 
+    private static final Logger logger = LoggerFactory.getLogger(VeiculoController.class);
+
+    private final VeiculoService veiculoService;
+
+    public VeiculoController(VeiculoService veiculoService) {
+        this.veiculoService = veiculoService;
+    }
+
+
+    // Using query params: http://localhost:8080/veiculos?page=1&size=10 => page = 1, size = 10
     @GetMapping
-    public String getVeiculos(){
-        return "Hello veiculo!";
+    public ResponseEntity<List<Veiculo>> findAllVeiculos(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size
+    ) {
+        logger.info("Foi acessado o endpoint de veiculos /veiculos");
+        var veiculos = this.veiculoService.findAllVeiculos(page, size);
+        return ResponseEntity.ok(veiculos);
+    }
+
+    // http://localhost:8080/veiculos/1
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Veiculo>> findVeiculo(
+            @PathVariable("id") Long id
+    ) {
+        logger.info("/veiculos/" + id);
+        var veiculo = this.veiculoService.findVeiculoById(id);
+        return ResponseEntity.ok(veiculo);
     }
 }
+
+
