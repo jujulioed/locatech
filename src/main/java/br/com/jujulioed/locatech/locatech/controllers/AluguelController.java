e6fe2f75abe81091d91cced9d56ad76c25f3c223
jujulioed <julioeduardon9@hotmail.com>
2025-05-11 20:34:30 -0300

    Create CRUD for Rent entity (Alugueis)
    Signed-off-by: jujulioed <julioeduardon9@hotmail.com>

---
+++ src/main/java/br/com/jujulioed/locatech/locatech/controllers/AluguelController.java
@@ -0,0 +1,73 @@
+package br.com.jujulioed.locatech.locatech.controllers;
+
+import br.com.jujulioed.locatech.locatech.entities.Aluguel;
+import br.com.jujulioed.locatech.locatech.services.AluguelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AluguelController {
+    private static final Logger logger = LoggerFactory.getLogger(AluguelController.class);
+
+    private final AluguelService aluguelService;
+
+    public AluguelController(AluguelService aluguelService) {
+        this.aluguelService = aluguelService;
+    }
+
+    // Using query params: http://localhost:8080/alugueis?page=1&size=10 => page = 1, size = 10
+    @GetMapping
+    public ResponseEntity<List<Aluguel>> findAllAlugueis(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size
+    ) {
+        logger.info("Foi acessado o endpoint de alugueis /alugueis");
+        var alugueis = this.aluguelService.findAllAlugueis(page, size);
+        return ResponseEntity.ok(alugueis);
+    }
+
+    // http://localhost:8080/alugueis/1
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Aluguel>> findAlugueis(
+            @PathVariable("id") Long id
+    ) {
+        logger.info("/alugueis/" + id);
+        var aluguel = this.aluguelService.findAluguelById(id);
+        return ResponseEntity.ok(aluguel);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveAluguel(
+            @RequestBody Aluguel aluguel
+    ){
+        logger.info("POST =>  /alugueis");
+        this.aluguelService.saveAluguel(aluguel);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateAluguel(
+            @PathVariable("id") Long id,
+            @RequestBody Aluguel aluguel
+    ){
+        logger.info("PUT => /alugueis/" + id);
+        this.aluguelService.updateAluguel(aluguel, id);
+        var status = HttpStatus.NO_CONTENT;
+        return ResponseEntity.status(status.value()).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAluguel(
+            @PathVariable("id") Long id
+    ){
+        logger.info("DELETE => /alugueis/"+ id);
+        this.aluguelService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
